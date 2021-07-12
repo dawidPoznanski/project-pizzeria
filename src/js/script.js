907a079f295fd885f1ff19200c46c9bb9a88b065
Dawid Poznański <dawid.poznanski@outlook.com>
2021-07-12 21:35:38 +0200

    add images animation and control

--- src/js/script.js
+++ src/js/script.js
@@ -86,6 +86,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     } 
     initAccordion(){
       const thisProduct = this;
@@ -151,7 +152,8 @@
           console.log('optionId', optionId, 'option', option);
 
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
+          const optionSelected = (formData[paramId] && formData[paramId].includes(optionId));
+          if(optionSelected){
             // check if the option is not default
             if(!option.default == true) {
               // add option price to price variable
@@ -162,6 +164,18 @@
             if(option.default == true) {
               // reduce price variable
               price-= option.price;
+            }
+          }
+          
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          
+
+          if(optionImage){
+            if(optionSelected){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            }
+            else{
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
             }
           }
         }
