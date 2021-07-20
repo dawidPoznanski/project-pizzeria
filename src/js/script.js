f371825fbdfc0800ff44f4324f0051046ad81259
Dawid Poznański <dawid.poznanski@outlook.com>
2021-07-20 20:46:04 +0200

    add control to widget in cart

--- src/js/script.js
+++ src/js/script.js
@@ -385,10 +385,48 @@
 
       thisCart.dom.productList.appendChild(generatedDOM);
       console.log('adding product', menuProduct);
+
+      thisCart.products.push(new CartProduct(menuProduct, generatedDOM));
+      console.log('thisCart.products', thisCart.products)
     }
   }
   class CartProduct{
+    constructor(menuProduct, element){
+      const thisCartProduct = this;
     
+      thisCartProduct.id = menuProduct.id
+      thisCartProduct.name = menuProduct.name,
+      thisCartProduct.amount = menuProduct.amount,
+      thisCartProduct.amountWidget = menuProduct.amountWidget,
+      thisCartProduct.priceSingle = menuProduct.priceSingle,
+      thisCartProduct.price = menuProduct.price,
+      thisCartProduct.parms = menuProduct.parms,
+
+      thisCartProduct.getElements(element);
+      thisCartProduct.initAmountWidget();
+      console.log('thisCartProduct', thisCartProduct);
+    }
+    getElements(element){
+      const thisCartProduct = this;
+
+      thisCartProduct.dom = {};
+
+
+      thisCartProduct.dom.wrapper = element;
+      thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
+      thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
+      thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
+      thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
+    }
+    initAmountWidget() {
+      const thisCartProduct = this;
+      thisCartProduct.amountWidget = new AmountWidget(thisCartProduct.dom.amountWidget);
+      thisCartProduct.dom.amountWidget.addEventListener('updated', function () {
+        thisCartProduct.amount = thisCartProduct.amountWidget.value;
+         thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
+         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+       });
+    }
   }
   const app = {
     initMenu: function(){
